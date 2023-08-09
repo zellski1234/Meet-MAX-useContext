82cad9d8367b7b8cbd789cda33317b2379ed85ff
zellski1234 <zellski1234@gmail.com>
2023-08-09 05:08:43 +0100

    removed all usercontext

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
 import React from 'react'
 import './App.css';
-import ComponentC from './components/ComponentC';
-
-export const UserContext = React.createContext()
-export const ChannelContext = React.createContext()
 
 function App() {
   return (
     <div className="App">
-      <UserContext.Provider value={'Vishwas'}>
-        <ChannelContext.Provider value={'Codevolution'}>
-          <ComponentC />    
-        </ChannelContext.Provider>
-      </UserContext.Provider>
+    
     </div>
   );
 }
