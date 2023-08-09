1d7b1ed85ade7ff531c459b9dfb0aff4cdd94a2a
zellski1234 <zellski1234@gmail.com>
2023-08-09 09:34:36 +0100

    added form file

---
+++ src/components/UserForm.js
@@ -0,0 +1,12 @@
+import React, {useState} from 'react'
+
+function UserForm() {
+    const [firstName, setFirstName] = useState('')
+    const [lastName, setLastName] = useState('')
+
+  return (
+    <div>UserForm</div>
+  )
+}
+
+export default UserForm
