a668438574e066d1d9b2dce6455571ddde53c236
zellski1234 <zellski1234@gmail.com>
2023-08-09 04:28:59 +0100

    added new components

---
+++ src/components/ComponentC.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ComponentC() {
+  return (
+    <div>ComponentC</div>
+  )
+}
+
+export default ComponentC
---
+++ src/components/ComponentE.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ComponentE() {
+  return (
+    <div>ComponentE</div>
+  )
+}
+
+export default ComponentE
