29dfb513643e54240f3127faf4e9ae69a499a9a1
zellski1234 <zellski1234@gmail.com>
2023-08-09 09:21:38 +0100

    added two counters

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import React from 'react'
 import './App.css';
 import DocTitleOne from './components/DocTitleOne';
 import DocTitleTwo from './components/DocTitleTwo';
+import CounterOne from './components/CounterOne';
+import CounterTwo from './components/CounterTwo';
 
 function App() {
   return (
     <div className="App">
       <DocTitleOne />  
       <DocTitleTwo />
+
+      <CounterOne />
+      <CounterTwo />
     </div>
   );
 }
---
+++ src/components/CounterTwo.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react'
+
+function CounterTwo() {
+    const [count, setCount] = useState(0)
+    const increment = () => {
+        setCount(prevCount => prevCount + 1)
+    }
+
+    const decrement = () => {
+        setCount(prevCount => prevCount - 1)
+    }
+
+    const reset = () => {
+        setCount(0)
+    }
+  return (
+    <div>
+        <h2> Count = {count} </h2>
+        <button onClick={increment}>Increment</button>
+        <button onClick={decrement}>Decrement</button>
+        <button onClick={reset}>Reset</button>
+    </div>
+  )
+}
+
+export default CounterTwo
