5af448ac3954be9c49bd038b81663509253fef16
zellski1234 <zellski1234@gmail.com>
2023-08-09 08:23:21 +0100

    added useRef

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import './App.css';
+import Focusinput from './components/Focusinput';
 
 
 function App() {
   return (
     <div className="App">
-
+      <Focusinput />
     </div>
   );
 }
