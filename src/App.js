eb89d524b80879beae672f4a9097482a493d3562
zellski1234 <zellski1234@gmail.com>
2023-08-09 07:31:34 +0100

    fetched using states

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import './App.css';
+import DataFetchingOne from './components/DataFetchingOne';
 
 
 function App() {
   return (
     <div className="App">
- 
+      <DataFetchingOne />
     </div>
   );
 }
--- src/components/DataFetchingOne.js
+++ src/components/DataFetchingOne.js
@@ -1,12 +1,31 @@
-import React, { useState } from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 
 function DataFetchingOne() {
     const [loading, setLoading] = useState(true)
     const [error, setError] = useState('true')
     const [post, setPost] = useState({})
+
+    useEffect(() => {
+      axios.get('https://jsonplaceholder.typicode.com/posts/1')
+      .then(res => {
+        setLoading(false)
+        setPost(res.data)
+        setError('')
+      })
+      .catch(error => {
+        setLoading(false)
+        setPost({})
+        setError('something went wrong')
+        console.log(error)
+      })
+    }, [])
     
   return (
-    <div>DataFetchingOne</div>
+    <div>
+        {loading ? 'Loading' : post.title }
+        {error ? error : null}
+    </div>
   )
 }
 
