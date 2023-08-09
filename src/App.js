aa7d1a70a0dc3aa477edbe6f600cc0ff51264b41
zellski1234 <zellski1234@gmail.com>
2023-08-09 08:49:06 +0100

    added class timer and hook timer using useRef

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import './App.css';
 import Focusinput from './components/Focusinput';
+import ClassTimer from './components/ClassTimer';
+import HookTimer from './components/HookTimer';
 
 
 function App() {
   return (
     <div className="App">
       <Focusinput />
+      <ClassTimer />
+      <HookTimer />
     </div>
   );
 }
---
+++ src/components/HookTimer.js
@@ -0,0 +1,25 @@
+import React, { useEffect, useRef, useState } from 'react'
+
+function HookTimer() {
+    const [timer, setTimer] = useState(0)
+    const intervalRef = useRef()
+
+    useEffect(() => {
+      intervalRef.current = setInterval(() => {
+        setTimer(prevTimer => prevTimer + 1)
+      }, 1000)
+    
+      return () => {
+        clearInterval(intervalRef.current)
+      }
+    }, [])
+    
+  return (
+    <div>
+        HookTimer: {timer}s
+        <button onClick={()=> clearInterval(intervalRef.current)}>Clear Hook Timer</button>
+    </div>
+  )
+}
+
+export default HookTimer
