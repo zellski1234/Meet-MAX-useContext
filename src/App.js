f7244a3e28ba5213d4b951754328724853ec4658
zellski1234 <zellski1234@gmail.com>
2023-08-09 06:43:05 +0100

    added reducer part

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
-import React from 'react'
+import React, { useReducer } from 'react'
 import './App.css';
 import ComponentC from './components/ComponentA';
 import ComponentA from './components/ComponentA';
 import ComponentB from './components/ComponentB';
 
+const intialState = 0
+const reducer = (state, action) =>{
+    switch (action) {
+        case 'increment':
+            return state + 1            
+        case 'decrement':
+            return state - 1            
+        case 'reset':
+            return intialState         
+        default:
+            return state
+    }
+    
+
+}
+
+
 function App() {
+  const [count, dispatch] = useReducer(reducer, intialState)
   return (
     <div className="App">
+      Count: {count}
       <ComponentA />
       <ComponentB />
       <ComponentC />
