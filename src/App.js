eb1a976b859ba111107641068b4e57f02fe31142
zellski1234 <zellski1234@gmail.com>
2023-08-09 09:05:32 +0100

    added counter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import './App.css';
+import DocTitleOne from './components/DocTitleOne';
 
 function App() {
   return (
     <div className="App">
-  
+      <DocTitleOne />  
     </div>
   );
 }
---
+++ src/components/DocTitleOne.js
@@ -0,0 +1,18 @@
+import React, { useEffect } from 'react'
+
+function DocTitleOne() {
+    const [count, setCount] = useState(0)
+
+    useEffect(() => {
+        document.title = `Count: ${count}`
+    }, [count])
+  return (
+    <div>
+        <button onClick={() => setCount(count + 1)}>Count: {count} </button>
+    </div>
+  )
+}
+
+
+
+export default DocTitleOne
